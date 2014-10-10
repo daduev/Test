554bd51a6410063ff7d1679894f615729953637e
Adam <daduev.ad@gmail.com>
2014-10-10 23:15:46 +0300

    c

--- src/com/home/Main02.java
+++ src/com/home/Main02.java
@@ -3,6 +3,7 @@ package com.home;
 
 public class Main02 {
 	public static void main(String[] args) {
+		System.out.println("Main02");
 		double d = 566.12;
 		long l = Math.round(d);
 		int i = (int) (l % 100);
