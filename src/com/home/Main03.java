8eb23bcfe743f03b5be6d433f00f53282d8654dc
Adam <daduev.ad@gmail.com>
2014-10-10 23:23:18 +0300

    c

--- src/com/home/Main03.java
+++ src/com/home/Main03.java
@@ -5,7 +5,7 @@ import java.math.BigDecimal;
 public class Main03 {
 
 	public static void main(String[] args) {
-		System.out.println("Main03");
+		System.out.println("Main033");
 	    BigDecimal bdTest = new BigDecimal(0.7455);
         BigDecimal bdTest1 = new BigDecimal("0.7457");
         bdTest = bdTest.setScale(3, BigDecimal.ROUND_HALF_UP);
