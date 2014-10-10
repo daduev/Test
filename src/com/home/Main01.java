7974e1fc5e5e0b926ff13f8a06f173d152940eb9
Adam <daduev.ad@gmail.com>
2014-10-10 23:10:07 +0300

    commit

--- src/com/home/Main01.java
+++ src/com/home/Main01.java
@@ -7,6 +7,7 @@ import java.util.Date;
 public class Main01 {
 
 	public static void main(String[] args) {
+		System.out.println("Main01");
 		RuntimeMXBean bean = ManagementFactory.getRuntimeMXBean();
 		System.out.println("-----------------------uptime " + new Date(bean.getUptime()));
 	}
